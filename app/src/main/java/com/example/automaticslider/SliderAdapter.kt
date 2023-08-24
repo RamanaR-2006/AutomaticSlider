9f9d35beae345a8e16136a86ecc381b0a8848dc9
RamanaR-2006 <ramanaramachandran14@gmail.com>
2023-08-24 21:51:01 +0530

    a

---
+++ app/src/main/java/com/example/automaticslider/SliderAdapter.kt
@@ -0,0 +1,13 @@
+package com.example.automaticslider
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SliderAdapter(fragmentActivity: FragmentActivity, private val fragments: List<Fragment>) :
+    FragmentStateAdapter(fragmentActivity) {
+
+    override fun getItemCount(): Int = fragments.size
+
+    override fun createFragment(position: Int): Fragment = fragments[position]
+}
